9df6e9564ff73ebbb5f3ed5ffd017e85ff95419b
“Robert <robert.rapkowski@tauceti.email>
2022-11-03 18:54:09 +0100

    - Explore button enabled when scrolling - Animation for next slide added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let controller;
 let slideScene;
+let pageScene;
 
 function animateSlides() {
 	// Init controller
@@ -10,22 +11,43 @@ function animateSlides() {
 
 	// Loop over each slide
 
-	sliders.forEach((slide) => {
+	sliders.forEach((slide, index, slides) => {
 		const revealImg = slide.querySelector(".reveal-img");
 		const img = slide.querySelector("img");
 		const revealText = slide.querySelector(".reveal-text");
 		// GSAP
-		const slideTimeline = gsap.timeline({ defaults: { duration: 1, ease: "power2.inOut" } });
-		slideTimeline.fromTo(revealImg, { x: "0" }, { x: "100%" });
-		slideTimeline.fromTo(img, { scale: 2 }, { scale: 1 }, "-=1");
-		slideTimeline.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=0.75");
-		slideTimeline.fromTo(nav, { y: "-70%" }, { y: "0" }, "-=0.5");
+		const timeline1 = gsap.timeline({ defaults: { duration: 1, ease: "power2.inOut" } });
+		timeline1.fromTo(revealImg, { x: "0" }, { x: "100%" });
+		timeline1.fromTo(img, { scale: 2 }, { scale: 1 }, "-=1");
+		timeline1.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=0.75");
+		timeline1.fromTo(nav, { y: "-70%" }, { y: "0" }, "-=0.5");
 		// Scene
 		slideScene = new ScrollMagic.Scene({
 			triggerElement: slide,
 			triggerHook: 0.25,
+			reverse: false,
 		})
-			.addIndicators({ colorStart: "white", colorTrigger: "white", name: "slide" })
+			.setTween(timeline1)
+
+			.addTo(controller);
+		// New animation
+		const timeline2 = gsap.timeline();
+		// Checking if it's the last element or not
+		let nextSlide = slides.length - 1 === index ? "end" : slides[index + 1];
+		timeline2.fromTo(nextSlide, { y: "0%" }, { y: "50%" });
+
+		timeline2.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 0.5 });
+		timeline2.fromTo(nextSlide, { y: "50%" }, { y: "0%" }, "-=0.5");
+		// New sceene
+		pageScene = new ScrollMagic.Scene({
+			triggerElement: slide,
+			duration: "100%",
+			triggerHook: 0,
+		})
+
+			//{ pushFollowers: false } helps get rid of the empty space and start the nast slide animation
+			.setPin(slide, { pushFollowers: false })
+			.setTween(timeline2)
 			.addTo(controller);
 	});
 }
