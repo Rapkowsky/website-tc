e46bdf3e062acd0882dd8f165d08092260e85861
“Robert <robert.rapkowski@tauceti.email>
2022-11-06 18:56:42 +0100

    - Page transition added - Logo href fied

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 let controller;
 let slideScene;
 let pageScene;
+
 // Vars
 const mouse = document.querySelector(".cursor");
 const mouseTxt = mouse.querySelector("span");
 const burger = document.querySelector(".burger");
+const logo = document.querySelector("#logo");
 // Listeners
-window.addEventListener("mousemove", cursorAnimation);
-window.addEventListener("mouseover", activeCursor);
 burger.addEventListener("click", navToggle);
+window.addEventListener("mousemove", cursor);
+window.addEventListener("mouseover", activeCursor);
 
 function animateSlides() {
 	// Init controller
@@ -24,43 +26,53 @@ function animateSlides() {
 		const img = slide.querySelector("img");
 		const revealText = slide.querySelector(".reveal-text");
 		// GSAP
-		const timeline1 = gsap.timeline({ defaults: { duration: 1, ease: "power2.inOut" } });
-		timeline1.fromTo(revealImg, { x: "0" }, { x: "100%" });
-		timeline1.fromTo(img, { scale: 2 }, { scale: 1 }, "-=1");
-		timeline1.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=0.75");
-		timeline1.fromTo(nav, { y: "-70%" }, { y: "0" }, "-=0.5");
-		// Scene
+		const slideTl = gsap.timeline({ defaults: { duration: 1, ease: "power2.inOut" } });
+		slideTl.fromTo(revealImg, { x: "0" }, { x: "100%" });
+		slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, "-=1");
+		slideTl.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=0.75");
+		slideTl.fromTo(nav, { y: "-70%" }, { y: "0%" }, "-=0.75");
+		// Create the Scene
 		slideScene = new ScrollMagic.Scene({
 			triggerElement: slide,
 			triggerHook: 0.25,
 			reverse: false,
 		})
-			.setTween(timeline1)
-
+			.setTween(slideTl)
+			.addIndicators({
+				colorStart: "white",
+				colorTrigger: "white",
+				name: "slide",
+			})
 			.addTo(controller);
 		// New animation
-		const timeline2 = gsap.timeline();
+		const pageTl = gsap.timeline();
 		// Checking if it's the last element or not
 		let nextSlide = slides.length - 1 === index ? "end" : slides[index + 1];
-		timeline2.fromTo(nextSlide, { y: "0%" }, { y: "50%" });
+		pageTl.fromTo(nextSlide, { y: "0%" }, { y: "50%" });
 
-		timeline2.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 0.5 });
-		timeline2.fromTo(nextSlide, { y: "50%" }, { y: "0%" }, "-=0.5");
+		pageTl.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 0.5 });
+		pageTl.fromTo(nextSlide, { y: "50%" }, { y: "0%" }, "-=0.5");
 		// New sceene
 		pageScene = new ScrollMagic.Scene({
 			triggerElement: slide,
 			duration: "100%",
 			triggerHook: 0,
 		})
+			.addIndicators({
+				colorStart: "white",
+				colorTrigger: "white",
+				name: "page",
+				indent: 200,
+			})
 
 			//{ pushFollowers: false } helps get rid of the empty space and start the nast slide animation
 			.setPin(slide, { pushFollowers: false })
-			.setTween(timeline2)
+			.setTween(pageTl)
 			.addTo(controller);
 	});
 }
 
-function cursorAnimation(e) {
+function cursor(e) {
 	mouse.style.top = e.pageY + "px";
 	mouse.style.left = e.pageX + "px";
 }
@@ -102,5 +114,46 @@ function navToggle(e) {
 		document.body.classList.remove("hide");
 	}
 }
-
-animateSlides();
+//  Barba page transitions
+barba.init({
+	views: [
+		{
+			namespace: "home",
+			beforeEnter() {
+				animateSlides();
+				logo.href = "./index.html";
+			},
+			beforeLeave() {
+				slideScene.destroy();
+				pageScene.destroy();
+				controller.destroy();
+			},
+		},
+		{
+			namespace: "fashion",
+			beforeEnter() {
+				logo.href = "../index.html";
+				gsap.fromTo(".nav-header", 2, { y: "-100%" }, { y: "0%", ease: "power2.inOut" });
+			},
+		},
+	],
+	transitions: [
+		{
+			leave({ current, next }) {
+				let done = this.async();
+				// Scroll to the top
+				window.scrollTo(0, 0);
+				// Animation
+				const tl = gsap.timeline({ defaults: { ease: "power2.inOut" } });
+				tl.fromTo(current.container, 1, { opacity: 1 }, { opacity: 0 });
+				tl.fromTo(".swipe", 0.75, { x: "-100%" }, { x: "0%", onComplete: done }, "-=1");
+			},
+			enter({ current, next }) {
+				let done = this.async();
+				const tl = gsap.timeline({ defaults: { ease: "power2.inOut" } });
+				tl.fromTo(".swipe", 0.75, { x: "0%" }, { x: "100%", stagger: 0.15, onComplete: done });
+				tl.fromTo(next.container, 1, { opacity: 0 }, { opacity: 1 });
+			},
+		},
+	],
+});
